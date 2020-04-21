fcd2cde79bc6de5dbe60e4e5e0a6d218856d4d68
Omar Hadhoud <OmarHadhoud@ymail.com>
2020-04-21 13:51:32 +0200

    Fixed single layer test bench to take more parameters

--- project_1/project_1.srcs/sources_1/new/SingleLayer_tb.v
+++ project_1/project_1.srcs/sources_1/new/SingleLayer_tb.v
@@ -1,11 +1,11 @@
 `timescale 1ns / 1ps
 module SingleLayer_tb;
 parameter DATA_WIDTH = 32 ; 
-parameter INPUT_SIZE= 32; //Number of inputs
-parameter OUTPUT_SIZE = 10 ; //Number of outputs
+parameter INPUT_SIZE= 100; //Number of inputs
+parameter OUTPUT_SIZE = 32 ; //Number of outputs
 parameter ADDR_WIDTH = 9 ; 
-parameter file = "E:/VivadoFiles/Weights_FC_0_ones.txt";
-parameter inputs_file_0 = "E:/VivadoFiles/Inputs_FC_0.txt";
+parameter file = "C:/Users/giga/Documents/Weights_FC_0_ones.txt";
+parameter inputs_file_0 = "C:/Users/giga/Documents/Inputs_FC_0.txt";
 reg  [DATA_WIDTH*INPUT_SIZE-1:0] input_fc;
 reg clk, start_fc, reset,done;
 wire [DATA_WIDTH*OUTPUT_SIZE-1:0] output_fc_final;
@@ -20,7 +20,8 @@ reg [DATA_WIDTH-1:0] inputs_mem [0:INPUT_SIZE-1] ; // inputs_mem[0] --> 32bit
     $readmemh(inputs_file_0, inputs_mem); // memory_list is memory file
     for( i = 0;i <INPUT_SIZE;i=i+1)
         input_fc[i*DATA_WIDTH+:DATA_WIDTH] = inputs_mem[i];
-    reset=1; 
+    reset=0; 
+    #1 reset = 1;
     start_fc=1;
     clk = 0;
     done =0; 
@@ -33,7 +34,7 @@ reg [DATA_WIDTH-1:0] inputs_mem [0:INPUT_SIZE-1] ; // inputs_mem[0] --> 32bit
     always begin
         #5  clk = ~clk; // Toggle clock every 5 ticks 
      end
-SingleLayer #(.INPUT_SIZE(INPUT_SIZE), .file(file))//instantiate the module                                                     
+SingleLayer #(.INPUT_SIZE(INPUT_SIZE), .file(file), .OUTPUT_SIZE(OUTPUT_SIZE))//instantiate the module                                                     
 SL( .input_fc(input_fc),
     .clk(clk),
     .start_fc(start_fc),
