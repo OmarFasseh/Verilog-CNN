ecb87f3b1d7c95d99e05e1fcd912a37fdffbaa2a
Omar Hadhoud <OmarHadhoud@ymail.com>
2020-05-19 23:41:32 +0200

    parameterized fc layer ann

--- FC_Layer_ANN.v
+++ FC_Layer_ANN.v
@@ -3,7 +3,11 @@ module FC_Layer_ANN(
 input_fc,weightCaches_fc, clk,start_FC,output_fc,test_multi
 );
 
-parameter DATA_WIDTH = 32 ; //ieee float = 32
+
+parameter EXPONENT_WIDTH = 8;
+parameter MANTISSA_WIDTH = 23;
+parameter DATA_WIDTH = EXPONENT_WIDTH+MANTISSA_WIDTH+1;
+
 parameter parallel_fc_PE= 32; //number of outputs (cols)
 parameter DATA_WIDTH_OUT = 32 ; //float
 input [DATA_WIDTH-1:0] input_fc;
@@ -16,7 +20,8 @@ genvar i;
 generate  //generate the parallel processing elements
 	for (i=0; i < parallel_fc_PE; i=i+1) 
 	begin :PE 
-	PE_FC_ANN PEs (
+	PE_FC_ANN #(.DATA_WIDTH(DATA_WIDTH), .EXPONENT_WIDTH(EXPONENT_WIDTH), .MANTISSA_WIDTH(MANTISSA_WIDTH)) 
+	PEs (
 	 .input_fc   (input_fc),
 	 .iweight_FC (weightCaches_fc[DATA_WIDTH*i+:DATA_WIDTH]),
 	 .clk(clk),.start_FC(start_FC),
