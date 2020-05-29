56f023f8c92b6613e0708eb0ba8b1a276836edea
Omar Fasseh <okomarkhaled@gmail.com>
2020-05-29 14:14:45 +0200

    fixed issue with directories

--- project_1/project_1.srcs/sources_1/new/LeNet.v
+++ project_1/project_1.srcs/sources_1/new/LeNet.v
@@ -4,6 +4,14 @@ parameter EXPONENT_WIDTH = 8;
 parameter MANTISSA_WIDTH = 23;
 parameter DATA_WIDTH = EXPONENT_WIDTH+MANTISSA_WIDTH+1;
 reg clk;
+//PC_MF 
+parameter N = 32; //The size of the image NxN
+parameter filters_number = 2; //Number of filters
+reg [N*N*DATA_WIDTH-1:0] PC_image;
+reg [25*filters_number*DATA_WIDTH-1:0] PC_filters;
+reg PC_reset;
+wire [(N-4)*(N-4)*filters_number*DATA_WIDTH-1:0] PC_output_fmap;
+wire  PC_done;
 //tanh
 parameter numberOfInputs=6; 
 reg [((EXPONENT_WIDTH+MANTISSA_WIDTH+1)*numberOfInputs)-1:0] tanh_input_value;
@@ -18,6 +26,15 @@ parameter numberOfExps=10;
 reg [(DATA_WIDTH*numberOfExps)-1:0] sm_input_value;
 wire [(numberOfExps*DATA_WIDTH)-1:0] sm_output_value;
 wire sm_done;
+
+PC_MF #(.EXPONENT_WIDTH(EXPONENT_WIDTH), .MANTISSA_WIDTH(MANTISSA_WIDTH), .numberOfInputs(numberOfInputs),.N(N),.filters_number(filters_number))
+PC_MF1(.image(PC_image),
+.filters(PC_filters),
+.reset(PC_reset),
+.clk(clk),
+.output_fmap(PC_output_fmap),
+.done(PC_done)
+);
 
 tanh_activation_function #(.EXPONENT_WIDTH(EXPONENT_WIDTH), .MANTISSA_WIDTH(MANTISSA_WIDTH), .numberOfInputs(numberOfInputs)) 
 tanh1 (
