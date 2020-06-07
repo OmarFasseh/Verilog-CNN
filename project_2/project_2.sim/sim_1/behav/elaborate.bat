292de874b35e5ae9db89681fc2cdf9e60bc64afb
Omar Fasseh <okomarkhaled@gmail.com>
2020-06-08 01:36:49 +0200

    cleared simulation files

--- project_2/project_2.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=E:\\Xilinx\\Vivado\\2015.2\\bin
-call %xv_path%/xelab  -wto 8c1ae150b43348a2b518771db0cdcfbf -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L unisims_ver -L unimacro_ver -L secureip --snapshot LeNet_tb_behav xil_defaultlib.LeNet_tb xil_defaultlib.glbl -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
